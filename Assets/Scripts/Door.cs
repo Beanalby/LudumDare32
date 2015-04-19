a98417da4e9d7445d52a05dc134fbc9fd6ef11b4
U-Beanolith\ninji <Jason Viers>
2015-04-19 19:23:15 -0400

    Added scale and door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    private float distScale = 1f;
+
+    private Vector3 basePos;
+    private Rigidbody2D rb;
+
+    public void Start() {
+        basePos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void SetScaleDelta(float dist) {
+        Vector3 pos = basePos;
+        pos.y += dist * distScale;
+        rb.MovePosition(pos);
+    }
+}
---
+++ Assets/Scripts/Scale.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LudumDare32 {
+    [RequireComponent(typeof(SpringJoint2D),typeof(SliderJoint2D))]
+    public class Scale : MonoBehaviour {
+
+        public GameObject target;
+        private SpringJoint2D spring;
+        private SliderJoint2D slider;
+        Rigidbody2D platform;
+        private float baseDistance;
+
+        public void Start() {
+            spring = GetComponent<SpringJoint2D>();
+            slider = GetComponent<SliderJoint2D>();
+            platform = spring.connectedBody;
+
+            // set default distance and limits based on what we're connected to
+            baseDistance = transform.position.y - platform.transform.position.y;
+            spring.distance = baseDistance - 1;
+            slider.useLimits = true;
+            slider.limits = new JointTranslationLimits2D() { min = -(baseDistance + 2), max = -(baseDistance - 1) };
+        }
+
+        public void Update() {
+            float delta = (transform.position.y - platform.transform.position.y) - baseDistance;
+            delta = Mathf.Max(0, delta);
+            target.SendMessage("SetScaleDelta", delta);
+        }
+    }
+}
