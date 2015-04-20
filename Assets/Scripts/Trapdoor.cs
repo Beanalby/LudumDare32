4b41d70ce478e52a2331693018155dd4b3358104
U-Beanolith\ninji <Jason Viers>
2015-04-19 22:33:58 -0400

    Added trapdoor

---
+++ Assets/Scripts/Trapdoor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trapdoor : MonoBehaviour {
+    private float openDistance = 1f;
+
+    private Rigidbody2D rb;
+
+    public void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void SetScaleDelta(float dist) {
+        Debug.Log("dist=" + dist);
+        dist = Mathf.Min(openDistance, dist);
+        float percent = dist / openDistance;
+        rb.MoveRotation(-90 * percent);
+    }
+}
