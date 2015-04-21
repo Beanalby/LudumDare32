fac45d4919d83e25cf31eb3e16ecd9083df07602
U-Beanolith\ninji <Jason Viers>
2015-04-20 20:20:48 -0400

    Added spawn points

--- Assets/Scripts/GameDriver.cs
+++ Assets/Scripts/GameDriver.cs
@@ -8,12 +8,41 @@ namespace LudumDare32 {
         }
         private static GameDriver _instance = null;
 
+        public string spawnName;
+
         public void Awake() {
-            if (_instance != null) {
-                Debug.LogError("Can't have two gameDrivers!");
+            if (_instance != null && Instance != this) {
+                // there was already a GameDriver, get rid of this one
                 Destroy(gameObject);
+                return;
             }
+            DontDestroyOnLoad(transform.gameObject);
             _instance = this;
+        }
+
+        public void Start() {
+            InitLevel();
+        }
+        public void OnLevelWasLoaded(int level) {
+            InitLevel();
+        }
+
+        public void InitLevel() {
+            if (spawnName != "") {
+                // move the player to the active spawn point
+                GameObject spawn = GameObject.Find(spawnName);
+                if (spawn == null) {
+                    Debug.LogError("Can't find spawnpoint [" + spawnName + "]!");
+                    Debug.Break();
+                    return;
+                }
+                Player player = GameObject.FindObjectOfType<Player>();
+                player.transform.position = spawn.transform.position;
+                // move the camera to the player immedaitely
+                Vector3 pos = Camera.main.transform.position;
+                pos.x = player.transform.position.x;
+                Camera.main.transform.position = pos;
+            }
         }
 
         public void PlayerDied() {
@@ -23,6 +52,9 @@ namespace LudumDare32 {
         private IEnumerator _playerDied() {
             yield return new WaitForSeconds(2);
             Application.LoadLevel(Application.loadedLevel);
+        }
+        public void OnDestroy() {
+            Debug.Log("GameDriver getting destroyed!");
         }
     }
 }
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LudumDare32 {
+    public class SpawnPoint : MonoBehaviour {
+
+        public void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.layer != LayerMask.NameToLayer("Player")) {
+                return;
+            }
+            Debug.Log(Time.time + " " + name + " hit " + other.name + ", setting myself to active spawn point");
+            GameDriver.Instance.spawnName = this.name;
+        }
+
+    }
+}
